2bf98977bee4cfaa99d111eaa8e6cda9318b6bac
Simon-Christian Klein <simoniusklein@gmail.com>
2018-10-11 11:17:49 +0200

    changed the codingstyle and some lines as requested in PR #33. Added the timer and OpenCl device querry from the matlab version. found a bug from Fortran vs C array indexing

--- python/cl.py
+++ python/cl.py
@@ -1,56 +1,62 @@
 import pyopencl as cl
 
 def compile_kernels(src_path, cmd_line, *args):
-	global program, ctx
-	src = ' '
-	for entry in src_path:
-		file = open(entry, 'r')
-		src = src + file.read()
-		file.close()
-	prg = cl.Program(ctx, src).build(options=cmd_line)
-	program = prg
-	return prg
+    global program, ctx
+    src = ' '
+    for entry in src_path:
+        with open(entry, 'r') as io:
+            src = src + io.read()
+    prg = cl.Program(ctx, src).build(options=cmd_line)
+    program = prg
+    return prg
 
 def create_ctx():
-	global ctx, queue, PYOPENCL_COMPILER_OUTPUT
-	PYOPENCL_COMPILER_OUTPUT=1
-	ctx = cl.create_some_context()
-	queue = cl.CommandQueue(ctx)
-	return ctx, queue
+    global ctx, queue, PYOPENCL_COMPILER_OUTPUT, devices, group_size
+    PYOPENCL_COMPILER_OUTPUT=1
+    ctx = cl.create_some_context()
+    queue = cl.CommandQueue(ctx)
+    devices = ctx.devices
+    group_size = devices[0].max_work_group_size
+    return ctx, queue
 
 def run_kernel(names, g_range, l_range, *arguments):
-	global queue, program
-	inputbuffers = []
-	for arg in arguments:
-		inputbuffers.append(cl.Buffer(ctx, cl.mem_flags.COPY_HOST_PTR, hostbuf = arg))
-	callstrings = []
-	if type(names) is list:
-		for name in names:
-			callstrings.append('program.' + name + '(queue, tuple(g_range) , None, *inputbuffers)')
-	else:
-		callstrings.append('program.' + names + '(queue, tuple(g_range), None, *inputbuffers)')
+    global queue, program
+    inputbuffers = []
+    for arg in arguments:
+        inputbuffers.append(cl.Buffer(ctx, cl.mem_flags.COPY_HOST_PTR, hostbuf = arg))
+    callstrings = []
+    if l_range[0] == 0 and len(l_range) == 1:
+        l_range = None
+    else:
+        l_range = tuple(l_range)
 
-	for callstring in callstrings:
-		event = eval(callstring)
-#		event.wait() 
-	i = 0
-	for arg in arguments:
-		cl.enqueue_copy(queue, arg, inputbuffers[i])
-#		inputbuffers[i].release()
-		i = i +1
-	return 0
+    if type(names) is list:
+        for name in names:
+            callstrings.append('program.' + name + '(queue, tuple(g_range) , l_range, *inputbuffers)')
+    else:
+        callstrings.append('program.' + names + '(queue, tuple(g_range), l_range, *inputbuffers)')
+
+    for callstring in callstrings:
+        event = eval(callstring)
+        event.wait()
+    i = 0
+    for arg in arguments:
+        cl.enqueue_copy(queue, arg, inputbuffers[i]).wait()
+        inputbuffers[i].release()
+        i = i +1
+    return 0
 
 def run_kernel_ref(name, g_range, l_range, *arg_refs):
-	global queue, program
-	callstring = 'program.' + name + '(queue, None, None, *inputbuffers)'
-	eval(callstring)
-	return
+    global queue, program
+    callstring = 'program.' + name + '(queue, None, None, *inputbuffers)'
+    eval(callstring)
+    return
 
 def allocate_buf(array):
-	return cl.Buffer(ctx, cl.mem_flags.COPY_HOST_PTR, hostbuf = array)
+    return cl.Buffer(ctx, cl.mem_flags.COPY_HOST_PTR, hostbuf = array)
 
 def malloc(size):
-	return cl.Buffer(ctx, size=size)
+    return cl.Buffer(ctx, size=size)
 
 
 
--- python/compute_error_rotation_2d.py
+++ python/compute_error_rotation_2d.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: <utf8> -*-
 
 #This project is licensed under the terms of the Creative Commons CC BY-NC-ND 3.0 license.
@@ -9,6 +10,7 @@
 #enabled, e.g. divergence cleaning.
 import InductionEq as induction_eq
 from math import pi
+import sys
 #prepare_vars() initializes containers (maps) in which all variables (label, value) are
 #stored. Variables are grouped together by purpose:
 #I_Mesh: Contains all variables concerning the discrete mesh, e.g.
@@ -30,7 +32,11 @@ from math import pi
 #or to switch between different discretizations. In case the key is written
 #in capital letters the program define will be set to the corresponding
 #value of the key.
-induction_eq.preparse_vars();
+if sys.version_info[0] < 3:
+    raise Exception("Must be using Python 3")
+
+
+induction_eq.preparse_vars()
 
 
 #global I_Mesh I_TI I_IEq I_DC I_Tech I_RunOps I_Results
@@ -67,17 +73,17 @@ I_TI['time_integrator'] = 'SSPRK33' #'KennedyCarpenterLewis2R54C';
 # Induction equation related variables
 #Specify how the three part of the linear induction equation shall be
 #computed.
-I_IEq['form_uibj'] = 'USE_UIBJ_PRODUCT'; # PRODUCT, SPLIT, CENTRAL
-I_IEq['form_source'] = 'USE_SOURCE_CENTRAL'; # CENTRAL, SPLIT, ZERO
-I_IEq['form_ujbi'] = 'USE_UJBI_CENTRAL'; # SPLIT, CENTRAL, PRODUCT
+I_IEq['form_uibj'] = 'USE_UIBJ_PRODUCT' # PRODUCT, SPLIT, CENTRAL
+I_IEq['form_source'] = 'USE_SOURCE_CENTRAL' # CENTRAL, SPLIT, ZERO
+I_IEq['form_ujbi'] = 'USE_UJBI_CENTRAL' # SPLIT, CENTRAL, PRODUCT
 #Enable or disable Hall-Term
-I_IEq['hall_term'] = 'NONE'; # NONE, USE_HALL
+I_IEq['hall_term'] = 'NONE' # NONE, USE_HALL
 #Enable or disable artificial dissipation
-I_IEq['dissipation'] = 'NONE'; # NONE, USE_ARTIFICIAL_DISSIPATION
+I_IEq['dissipation'] = 'NONE' # NONE, USE_ARTIFICIAL_DISSIPATION
 #Specify what kind of artifical dissipation shall be used
     #USE_ADAPTIVE_DISSIPATION, USE_FIRST_ORDER_DISSIPATION, USE_HIGH_ORDER_DISSIPATION
-I_IEq['dissipation_form'] = 'USE_HIGH_ORDER_DISSIPATION';
-I_IEq['HO_DISSIPATION_FACTOR'] = 1; # a constant (non-negative) factor adapting the influence of the high order dissipation
+I_IEq['dissipation_form'] = 'USE_HIGH_ORDER_DISSIPATION'
+I_IEq['HO_DISSIPATION_FACTOR'] = 1 # a constant (non-negative) factor adapting the influence of the high order dissipation
 #Additional parameters needed for adaptive dissipation. For typical values
 #see Svärd et al. (2009).
 I_IEq['MP2MIN'] = 1
@@ -93,31 +99,31 @@ I_DC['divergence_cleaning'] = False
 I_DC['divergence_cleaning_form'] = 'USE_LAPLACE_WIDE_STENCIL_LNS'
 #USE_LAPLACE_WIDE_STENCIL_LNS USE_LAPLACE_WIDE_STENCIL_DIRICHLET USE_LAPLACE_NARROW_STENCIL_DIRICHLET
 #Specify a threshold for the divergence norm
-I_DC['absolute_error_threshold'] = 1e-3;
+I_DC['absolute_error_threshold'] = 1e-3
 #The divergence cleaner will exit after max_iterations even if the error
 #threshold is not reached
-I_DC['max_iterations'] = 50;
+I_DC['max_iterations'] = 50
 
 
 # Technical details
 # Specify the OpenCL device that will be used for computation
-I_Tech['device'] = 1;
+I_Tech['device'] = 1
 
 # Switch between single floating point and double floating point precision
 I_Tech['REAL'] = 'double' # float
-I_Tech['REAL4'] = I_Tech['REAL'] + '4'; #Vector datatype
+I_Tech['REAL4'] = I_Tech['REAL'] + '4' #Vector datatype
 
 #Compiler based optimizations
 if I_Tech['REAL'] == 'float':
-    I_Tech['optimizations'] = ' -cl-mad-enable -cl-no-signed-zeros -cl-finite-math-only -cl-single-precision-constant';
+    I_Tech['optimizations'] = ' -cl-mad-enable -cl-no-signed-zeros -cl-finite-math-only -cl-single-precision-constant'
 else:
-    I_Tech['optimizations'] = ' -cl-mad-enable -cl-no-signed-zeros -cl-finite-math-only';
+    I_Tech['optimizations'] = ' -cl-mad-enable -cl-no-signed-zeros -cl-finite-math-only'
 
 
 # Options relevant for a run
 # Defines the order
-I_RunOps['order'] = 4; # 2, 4, 6
-I_RunOps['operator_form'] = 'classical'; # 'classical' or 'extended' operators
+I_RunOps['order'] = 4 # 2, 4, 6
+I_RunOps['operator_form'] = 'classical' # 'classical' or 'extended' operators
 # Specify the testcase. The name of the testcase has to be equal to the
 # name of a header file which contains a function describing the initial state.
 # Example testcases are:
@@ -131,35 +137,35 @@ I_RunOps['periodic'] = 'NONE'; # 'NONE', 'USE_PERIODIC'; must be set to 'USE_PER
 # 'x', 'y', 'z'
 # If you want to plot multiple cross sections use
 # 'xy', 'xz', 'yz', 'xyz'
-I_RunOps['plot_numerical_solution'] = '';
-I_RunOps['plot_analytical_solution'] = '';
-I_RunOps['plot_difference'] = '';
-I_RunOps['plot_divergence'] = '';
+I_RunOps['plot_numerical_solution'] = ''
+I_RunOps['plot_analytical_solution'] = ''
+I_RunOps['plot_difference'] = ''
+I_RunOps['plot_divergence'] = ''
 #If set to 1 the magnetic field will be saved to I_Results('field_b')
-I_RunOps['save_fields'] = False;
+I_RunOps['save_fields'] = False
 #If set to true the magnetic energy (L^2 norm) will be saved to I_Results('energy_over_time'), the
 #L2 errors (if available) to I_Results('L2error_B_over_time') & I_Results('L2error_divB_over_time')
 #and the time to I_Results('time')
-I_RunOps['save_integrals_over_time'] = False;
+I_RunOps['save_integrals_over_time'] = False
 
 #Initialize the magnetic field, the velocity field and the density field
 #according to the specified testcase. Also calculates and sets additional
 #variables, e.g. stepsize, timestep, local work group size, etc...
-(field_b_init, field_u_init, field_rho_init) = induction_eq.initialize();
+(field_b_init, field_u_init, field_rho_init) = induction_eq.initialize()
 
-print('Testcase: ' + I_RunOps['testcase'] +'\n')
-print('Order:' +str(I_RunOps['order'])+ '\n')
-print('Time integrator:' + I_TI['time_integrator'] + ' \n')
-print('DT: '+ str(I_TI['DT']) + '   N_STEPS: ' + str(I_TI['num_steps']) + '   FINAL_TIME: '+str(I_TI['final_time'])+'\n')
-print('DX: '+ str(I_Mesh['DX']) + '   NODES_X: '+str(I_Mesh['NODES_X']) +'\n')
-print('DY: '+ str(I_Mesh['DY']) + '   NODES_Y: '+str(I_Mesh['NODES_Y'])+'\n')
-print('DZ: '+ str(I_Mesh['DZ']) + '   NODES_Z: '+str(I_Mesh['NODES_Z'])+'\n')
-print('Dissipation: '+I_IEq['dissipation']+' \n')
-print('Divergence cleaning: '+str(I_DC['divergence_cleaning'])+' \n')
-print('REAL:'+ I_Tech['REAL']+'\n')
+print('Testcase: ' + I_RunOps['testcase'])
+print('Order:' + str(I_RunOps['order']))
+print('Time integrator:' + I_TI['time_integrator'])
+print('DT: '+ str(I_TI['DT']) + '   N_STEPS: ' + str(I_TI['num_steps']) + '   FINAL_TIME: '+str(I_TI['final_time']))
+print('DX: '+ str(I_Mesh['DX']) + '   NODES_X: '+str(I_Mesh['NODES_X']))
+print('DY: '+ str(I_Mesh['DY']) + '   NODES_Y: '+str(I_Mesh['NODES_Y']))
+print('DZ: '+ str(I_Mesh['DZ']) + '   NODES_Z: '+str(I_Mesh['NODES_Z']))
+print('Dissipation: '+I_IEq['dissipation'])
+print('Divergence cleaning: '+str(I_DC['divergence_cleaning']))
+print('REAL:'+ I_Tech['REAL'])
 
 # Takes the initialized fields and advances the solution in time
 induction_eq.compute_numerical_solution(field_b_init, field_u_init, field_rho_init);
 
-print('Divergence Norm: ' + str(I_Results['divergence_norm']) + ' Total Energy: '+str(I_Results['energy']) + '\n')
+print('Divergence Norm: ' + str(I_Results['divergence_norm']) + ' Total Energy: '+str(I_Results['energy']))
 print('Relative Error:' + str(100*I_Results['rel_err']) + '%\n\n')
